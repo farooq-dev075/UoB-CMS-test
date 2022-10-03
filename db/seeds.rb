ef9cbd97095f3f24e2497877dfc49d6e90c47688
Farooq Ahmad <farooqahmad.dev@gmail.com>
2022-10-03 13:34:10 +0500

    Update Seed file * Created 2 teams * Added the memebers in teams * Added the test coverage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 # Person Profile seed data
-PersonProfile.create(role_holder_name: 'Test Person 1', subtype: 'academic_profile', pure_portal_url: 'https://researchportal.bath.ac.uk/example')
-PersonProfile.create(role_holder_name: 'Test Person 2', subtype: 'academic_profile', pure_portal_url: 'https://researchportal.bath.ac.uk/example')
+person_1 = PersonProfile.create(role_holder_name: 'Test Person 1', subtype: 'academic_profile', pure_portal_url: 'https://researchportal.bath.ac.uk/example')
+person_2 = PersonProfile.create(role_holder_name: 'Test Person 2', subtype: 'academic_profile', pure_portal_url: 'https://researchportal.bath.ac.uk/example')
+
+academic_team = TeamProfile.find_or_create_by(title: 'Test team 1', subtype: 'academic_team')
+leadership_team = TeamProfile.find_or_create_by(title: 'Test team 2', subtype: 'leadership_team')
+
+academic_team.person_profiles << [person_1, person_2]
+leadership_team.person_profiles << [person_1, person_2]
--- test/models/team_profile_test.rb
+++ test/models/team_profile_test.rb
@@ -2,36 +2,47 @@ require 'test_helper'
 
 class TeamProfileTest < ActiveSupport::TestCase
   def setup
-    @team = FactoryBot.create(:team_profile)
+    @team_1 = FactoryBot.create(:team_profile)
+    @team_2 = FactoryBot.create(:team_profile)
+    @person_1 = FactoryBot.build(:person_profile)
+    @person_2 = FactoryBot.build(:person_profile)
   end
 
   def test_valid
-    assert @team.valid?
+    assert @team_1.valid?
   end
 
   def test_tilte_presence
-    @team.update(title: '')
-    refute @team.valid?, 'Title should have failed validation'
-    assert @team.errors.added?(:title, :blank)
+    @team_1.update(title: '')
+    refute @team_1.valid?, 'Title should have failed validation'
+    assert @team_1.errors.added?(:title, :blank)
   end
 
   def test_subtype_presence
-    @team.update(subtype: '')
-    refute @team.valid?, 'Subtype should have failed validation'
-    assert @team.errors.added?(:subtype, :blank), 'team should be invalid due to blank subtype'
-    assert @team.errors.added?(:subtype, :inclusion, value: @team.subtype), 'team should be invalid due to unknown subtype'
+    @team_1.update(subtype: '')
+    refute @team_1.valid?, 'Subtype should have failed validation'
+    assert @team_1.errors.added?(:subtype, :blank), 'team should be invalid due to blank subtype'
+    assert @team_1.errors.added?(:subtype, :inclusion, value: @team_1.subtype), 'team should be invalid due to unknown subtype'
   end
 
   def test_team_with_unknown_subtype_invalid
-    @team.subtype = Faker::String.random(length: 8)
-    refute @team.valid?, 'Expected Team Profile with unknown subtype to be invalid'
-    assert @team.errors.added?(:subtype, :inclusion, value: @team.subtype), 'team should be invalid due to unknown subtype'
+    @team_1.subtype = Faker::String.random(length: 8)
+    refute @team_1.valid?, 'Expected Team Profile with unknown subtype to be invalid'
+    assert @team_1.errors.added?(:subtype, :inclusion, value: @team_1.subtype), 'team should be invalid due to unknown subtype'
   end
 
   def test_title_uniquness
-    @team2 = FactoryBot.create(:team_profile)
-    @team2.update(title: @team.title)
-    refute @team2.valid?
-    assert @team2.errors.added?(:title, :taken, value: @team.title), 'title should be unique among teams'
+    @team_2.update(title: @team_1.title)
+    refute @team_2.valid?
+    assert @team_2.errors.added?(:title, :taken, value: @team_1.title), 'title should be unique among teams'
+  end
+
+  def test_has_many_person_profiles
+    @team_1.person_profiles << [@person_1, @person_2]
+    @team_2.person_profiles << [@person_1]
+    assert @team_1.person_profiles.count, 2
+    assert @team_2.person_profiles.count, 1
+    assert @person_1.team_profiles.count, 2
+    assert @person_2.team_profiles.count, 1
   end
 end
